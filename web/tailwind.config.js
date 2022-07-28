153e98196562a84a5f25778e9ecfc108eb6eb635
Jefferson Charlles <contato@jefferdeveloper.com>
2022-07-28 11:37:55 -0300

    chore:config twailwind with project theme

---
+++ web/tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      gray: {
+        50: '#f7f8fa',
+        100: '#E6E8EB',
+        200: '#AFB2B1',
+        500: '#808080',
+        800: '#494D4B'
+      },
+      green: {
+        500: '#04D361'
+      },
+      purple: {
+        300: '#9F75FF',
+        400: '#9164FA',
+        500: '#8257E5',
+        800: '#6F48C9'
+      }
+    },
+    fontFamily: {
+      'sans': ['Inter', 'system-ui', 'sans-serif'],
+      'display': ['Lexend', 'Oswald', 'sans-serif'],
+    },
+    plugins: [],
+  }
+}
